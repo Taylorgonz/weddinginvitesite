f6f10a9de3f42482073944cbafd39808e48d0de0
taylorgonz <hello@taylorgonz.com>
2022-02-25 14:38:41 -0500

    adding folder

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+import {useState} from 'react';
+import './App.css';
+
+
+
+function App() {
+  const []= useState();
+
+  return (
+    <div className="App">
+      <h1>Welcome To My Wedding Site!</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/util/firebase.js
@@ -0,0 +1,22 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "@firebase/firestore";
+import env from "react-dotenv"
+
+
+const firebaseConfig = {
+    apiKey: env.API_KEY,
+    authDomain: env.AUTH_DOMAIN,
+    databaseURL: env.DATABASE_URL,
+    projectId: env.PROJECT_ID,
+    storageBucket: env.STORAGE_BUCKET,
+    messagingSenderId: env.MESSAGE_SENDER_ID,
+    appId: env.APP_ID,
+    measurementId: env.MEASUREMENT_ID
+  };
+  
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  
+  export const db = getFirestore(app);
+  
+  
