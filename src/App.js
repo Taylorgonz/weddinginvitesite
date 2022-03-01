1751960f56047e68c331f703124e69eb3e04a255
taylorgonz <hello@taylorgonz.com>
2022-03-01 16:36:11 -0500

    images imported/styling

--- src/App.js
+++ src/App.js
@@ -3,27 +3,44 @@ import {useState, useEffect} from 'react';
 import './App.css';
 import {db} from "./firebase"
 import { collection, getDocs } from "firebase/firestore";
+import stamp from "./photos/stamp.png";
 
 
 
 function App() {
-  const [guest, setGuests]= useState([]);
+  const [guests, setGuests]= useState([]);
   const guestRef = collection(db, "guests");
 
 
   useEffect(() => {
     const getGuests = async () => {
       const data = await getDocs(guestRef);
-      console.log(data);
+      setGuests(data.docs.map((doc)=> ({...doc.data(), id: doc.id})))
     }
     
 
     getGuests();
   }, [])
+  console.log(guests)
 
   return (
+
     <div className="App">
-      <h1>Welcome To My Wedding Site!</h1>
+      <div className="header">
+        <img className="stamp" src={stamp}/>
+      </div>
+      <div className="mainCont">
+      <div className="imageCont">
+        <h1 className="imageTitle">
+        CELEBRATE WITH US!
+        </h1>
+        <p>Join us for free beer, food, fun and help us celebrate our love together!</p>
+      </div>
+      <div className="infoCont">
+
+      </div>
+      </div>
+      
     </div>
   );
 }
