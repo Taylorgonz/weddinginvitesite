1b063b0d35102d13c480e58df368addf58120245
taylorgonz <hello@taylorgonz.com>
2022-03-01 16:36:23 -0500

    same

--- src/firebase.js
+++ src/firebase.js
@@ -1,19 +1,20 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-
+import env from "react-dotenv"
 
 
 
 const firebaseConfig = {
  
-    apiKey: env.API_KEY,
-    authDomain: env.AUTH_DOMAIN,
-    databaseURL: env.DATABASE_URL,
-    projectId: env.PROJECT_ID,
-    storageBucket: env.STORAGE_BUCKET,
-    messagingSenderId: env.MESSAGE_SENDER_ID,
-    appId: env.APP_ID,
-    measurementId: env.MEASUREMENT_ID
+  
+    apiKey: env.REACT_APP_API_KEY,
+    authDomain: env.REACT_APP_AUTH_DOMAIN,
+    databaseURL: env.REACT_APP_DATABASE_URL,
+    projectId: env.REACT_APP_PROJECT_ID,
+    storageBucket: env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: env.REACT_APP_MESSAGE_SENDER_ID,
+    appId: env.REACT_APP_APP_ID,
+    measurementId: env.REACT_APP_MEASUREMENT_ID
   };
   
   // Initialize Firebase
